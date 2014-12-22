c7203421eb2345bfe257cb765da7d50ce682d545
Sébastien M-B <seb@essembeh.org>
2014-12-22 15:39:06 +0100

    iw

---
+++ src/devstudio/Seb201412.java
@@ -0,0 +1,47 @@
+package devstudio;
+
+import java.io.FileNotFoundException;
+import java.util.Calendar;
+
+import devstudio.model.Activity;
+import devstudio.model.Constants;
+import devstudio.model.Csv;
+import devstudio.model.MonthlyReport;
+import devstudio.model.Seb;
+
+public class Seb201412 {
+	public static void main(String[] args) throws FileNotFoundException {
+		MonthlyReport report = new MonthlyReport(2014, Calendar.NOVEMBER);
+		Seb seb = new Seb(Constants.BRONTES);
+
+		report.newDay(seb.dev()).addSubActivity(Activity.DAYOFF, Constants.HALF_DAY);
+		report.newDay(seb.dev());
+		report.newDay(seb.meeting1h(seb.dev()));
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dayOff());
+		report.newDay(seb.dayOff());
+		report.newDay(seb.dayOff());
+
+		report.newDay(seb.dayOff());
+		report.newDay(seb.dayOff());
+		report.newDay(seb.dayOff());
+
+		Csv.writeReport(report);
+	}
+}
