af6cb6467525f787d3736fa439df1c640826066c
Sébastien M-B <seb@essembeh.org>
2015-07-01 09:58:13 +0200

    update

--- src/devstudio/Seb201507.java
+++ src/devstudio/Seb201507.java
@@ -3,20 +3,28 @@ package devstudio;
 import java.io.FileNotFoundException;
 import java.util.Calendar;
 
-import devstudio.model.Activity;
 import devstudio.model.Constants;
 import devstudio.model.Csv;
-import devstudio.model.Day;
 import devstudio.model.MonthlyReport;
 import devstudio.model.Seb;
 
-public class Seb201506 {
+public class Seb201507 {
 	public static void main(String[] args) throws FileNotFoundException {
-		MonthlyReport report = new MonthlyReport(2015, Calendar.JUNE);
+		MonthlyReport report = new MonthlyReport(2015, Calendar.JULY);
 		Seb seb = new Seb(Constants.PROJECT_CASSANDRA);
 
 		report.newDay(seb.dev());
+		report.newDay(seb.meeting1h(seb.dev()));
 		report.newDay(seb.dev());
+
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+		report.newDay(seb.dev());
+
+		report.newDay(seb.dev());
+		report.newDay(seb.publicHoliday());
 		report.newDay(seb.dev());
 		report.newDay(seb.dev());
 		report.newDay(seb.dev());
@@ -29,16 +37,7 @@ public class Seb201506 {
 
 		report.newDay(seb.dev());
 		report.newDay(seb.dev());
-		report.newDay(new Day(Activity.OTHER)); // Innovation Summit
-		report.newDay(seb.dayOff());
-		report.newDay(seb.dayOff());
-
-		report.newDay(seb.dayOff());
-		report.newDay(seb.dayOff());
-		report.newDay(seb.dayOff());
-		report.newDay(seb.dayOff());
-		report.newDay(seb.dayOff());
-
+		report.newDay(seb.dev());
 		report.newDay(seb.dev());
 		report.newDay(seb.dev());
 
